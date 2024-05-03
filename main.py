3e81d8a84bb54ff1ed97606f220b5f1879c1a3b8
Marco Antonio Sanches <marcosanches.dev@gmail.com>
2024-05-03 09:20:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#from datetime import datetime
 from docxtpl import DocxTemplate
 import openpyxl
 
@@ -9,28 +8,23 @@ modelo = DocxTemplate('template_certificado.docx')
 wb = openpyxl.load_workbook('dados.xlsx')
 planilha = wb['Planilha1']  # nome da planilha onde estão os dados
 
-#hoje = datetime.now().strftime('%d/%m/%Y')
-
 # Itera sobre as linhas da planilha, começando da segunda linha para evitar o cabeçalho
-for linha in planilha.iter_rows(min_row=2, values_only=True):
+for col in planilha.iter_rows(min_row=2, values_only=True):
     dados = {
-        'TITULO': linha[0],
-        'AUTOR1': linha[1],
-        'AUTOR2': linha[2],
-        'MODALIDADE': linha[3],
-        'EVENTO': linha[4],
-        'ORGANIZADOR': linha[5],
-        'DATA_EVENTO': linha[6],
-        'DATA_EMISSAO': linha[7]
-        # adicione mais marcadores conforme necessário
+        'TITULO': col[0], # coluna A
+        'AUTOR1': col[1], # coluna B
+        'AUTOR2': col[2], # coluna C
+        'MODALIDADE': col[3], # coluna D
+        'EVENTO': col[4], # coluna E
+        'ORGANIZADOR': col[5], # coluna F
+        'DATA_EVENTO': col[6], # coluna G
+        'DATA_EMISSAO': col[7] # coluna H
     }
 
-    # Renderizar o modelo Word com os dados
-    #modelo = modelo_1 if linha[3] == 'xxx' else modelo_2
+    # Renderiza o modelo Word com os dados da planilha
     modelo.render(dados)
 
-    # Salvar o documento preenchido
-    #arquivo = 'arquivos/' + linha[3] + '/' + linha[0].replace('/', '_') + '.docx'
+    # Salva o documento preenchido
     arquivo = 'arquivos/' + linha[1] + '_' + linha[2] + '.docx'
     
     modelo.save(arquivo)
